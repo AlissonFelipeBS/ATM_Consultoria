dfd80bfbc872e9bdb5cb99887451da09f192f2be
AlissonFelipeBS <alisson.fbrandao@gmail.com>
2024-02-02 01:34:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/atmconsultoria/ui/sobre/SobreFragment.java
@@ -0,0 +1,50 @@
+package com.example.atmconsultoria.ui.sobre;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.atmconsultoria.R;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SobreFragment extends Fragment {
+
+    public SobreFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String descricao = "A RWR Consultoria & Assessoria é uma organização - dente outros acolhimentos - voltada para o atendimento às Administrações Tributárias Municipais (ATM)";
+
+        Element versao = new Element();
+        versao.setTitle("Versão 1.0");
+
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription(descricao)
+
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@atmconsultoria.com.br", "Envie um e-mail")
+                .addWebsite("https://www.google.com/", "Acesse nosso site")
+
+                .addGroup("Redes Sociais")
+                .addFacebook("kayara.pereira.5", "Facebook")
+
+                .addItem(versao)
+
+                .create();
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
+    }
+}
